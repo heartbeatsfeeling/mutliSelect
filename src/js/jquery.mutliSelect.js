e101f9c94270f3aec2075f28f98b72f7855f7f65
heartbeatsfeeling <499954367@qq.com>
2016-08-24 21:49:20 +0800

    add renderItem

--- src/js/jquery.mutliSelect.js
+++ src/js/jquery.mutliSelect.js
@@ -8,15 +8,16 @@
 	}
 }(window, function($) {
 	var pluginName = 'mutliSelect';
-	var MutliSelect = function($this,option) {
-		this.option=option;
-		this.$element=$this;
+	var MutliSelect = function($this, option) {
+		option.callback=option.callback||{};
+		this.option = option;
+		this.$element = $this;
 		this._init();
 	};
-	MutliSelect.prototype ={
+	MutliSelect.prototype = {
 		select: function(origin, target) {
-			
-		},	
+
+		},
 		unSelect: function() {
 
 		},
@@ -26,29 +27,77 @@
 		unSelectAll: function() {
 
 		},
-		_init:function(){
-			this.$element.append("<div class='"+pluginName+"'></div>");
+		_init: function() {
+			var html ="<div class='mutliSelect'>" +
+					"	<div class='mutliSelect-left'>" +
+					"		<div class=\"mutliSelect-left-search\">" +
+					"			<input type=\"text\" />" +
+					"		</div>" +
+					"		<div class=\"mutliSelect-left-item\">" +
+					"			<ul></ul>" +
+					"		</div>" +
+					"	</div>" +
+					"	<div class='mutliSelect-right'>" +
+					"		<div class=\"mutliSelect-right-search\">" +
+					"			<input type=\"text\" />" +
+					"		</div>" +
+					"		<div class=\"mutliSelect-right-item\">" +
+					"			<ul></ul>" +
+					"		</div>" +
+					"	</div>" +
+					"</div>";
+			this.$element.append(html);
+			this._renderLeft();
+			this._renderRight();
+		},
+		_renderItem:function(item){
+			var html="<li data-mutliSelect-id='"+item.id+"'>";
+			var itemTemplate=this.option.callback.itemTemplate
+			if(itemTemplate&&$.isFunction(itemTemplate)){
+				html+=itemTemplate(item)
+			}else{
+				html="<span>"+item.text+"</span>"
+			}
+			return html+="</li>"
 		},
 		_renderLeft: function() {
-
+			var data=$.grep((this.option.data||[]),function(item){
+				return item.selected
+			},true)
+			var i=0;
+			var length=data.length;
+			var html="";
+			for(i;i<length;i++){
+				html+=this._renderItem(data[i]);
+			};
+			this.$element.find('.mutliSelect-left-item').html(html);
 		},
 		_renderRight: function() {
-
+			var data=$.grep((this.option.data||[]),function(item){
+				return item.selected
+			},false)
+			var i=0;
+			var length=data.length;
+			var html="";
+			for(i;i<length;i++){
+				html+=this._renderItem(data[i]);
+			};
+			this.$element.find('.mutliSelect-right-item').html(html);
 		},
 		_renderTool: function() {
 
 		}
 	};
-	$.fn[pluginName] = function(option,params) {
+	$.fn[pluginName] = function(option, params) {
 		var defaultOption = {};
 		this.each(function() {
-			var $this=$(this);
-			var select =$this.data(pluginName);
-			if (typeof option === 'string'&&select[option]) {
+			var $this = $(this);
+			var select = $this.data(pluginName);
+			if (typeof option === 'string' && select[option]) {
 				select[option](params);
 			} else if (typeof option === 'object') {
 				option = $.extend(true, defaultOption, option);
-				$this.data(pluginName,new MutliSelect($this,option));
+				$this.data(pluginName, new MutliSelect($this, option));
 			};
 		});
 		return this;
